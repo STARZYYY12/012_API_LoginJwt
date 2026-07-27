cb69d6644b28beba70a187fad8b29581ba8ad0ef
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-07-27 14:54:32 +0700

    methode  get

--- index.js
+++ index.js
@@ -31,3 +31,12 @@ app.post('/komik', async (req, res) => {
         res.status(500).send({ error: err.message });
     }
 });
+
+app.get('/komik', async (req, res) => {
+    try {
+        const komik = await db.Komik.findAll();
+        res.send(komik);
+    } catch (err) {
+        res.send({ error: err.message });
+    }
+});
