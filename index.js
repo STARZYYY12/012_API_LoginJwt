1aee181a70dcf91b793e4a181f7c5b0204e996e5
STARZYYY12 <rizkyfaiz324@gmail.com>
2026-07-27 14:53:35 +0700

    index

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const db = require('./models');
+app.use(express.json());
+app.use(express.urlencoded({ 
+    extended: false
+}));
+
+app.
+listen(port, () => {
+    console.log(`Server is start on http://localhost:${3000}`);
+});
